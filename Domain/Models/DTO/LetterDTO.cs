346c78c55c1744ef1c50664a80e55b436b8ac5fc
Dmitry Mukharski <dmitry9678@gmail.com>
2026-07-28 14:26:39 +0300

    сделаны модели для цепочек сообщений

--- Domain/Models/DTO/LetterDTO.cs
+++ Domain/Models/DTO/LetterDTO.cs
@@ -6,13 +6,22 @@
         public Guid AddresseeId { get; set; }
         public UserDTO Recipient { get; set; }
         public Guid RecipientId { get; set; }
+
         public string AdresseeName { get; set; }
         public string AdresseeSurname { get; set; }
         public string AdresseeEmail { get; set; }
+
         public string RecipientName { get; set; }
         public string RecipientSurname { get; set; }
         public string RecipientEmail { get; set; }
+
         public DateTime SendTime { get; set; }
         public List<LetterStateDTO> LetterStates { get; set; }
+
+        public List<LetterDTO>? ChidLetters { get; set; }
+        public List<Guid>? ChildrenLettersGuids { get; set; }
+
+        public LetterDTO? ParentLetter { get; set; }
+        public Guid? ParentLetterGuid { get; set; }
     }
 }
---
+++ Persistence/Migrations/20260726132104_ParentAndChildLetters.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class ParentAndChildLetters : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "ChildLetterId",
+                table: "Letters",
+                type: "uuid",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "ParentLetterId",
+                table: "Letters",
+                type: "uuid",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Letters_ParentLetterId",
+                table: "Letters",
+                column: "ParentLetterId",
+                unique: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Letters_Letters_ParentLetterId",
+                table: "Letters",
+                column: "ParentLetterId",
+                principalTable: "Letters",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Letters_Letters_ParentLetterId",
+                table: "Letters");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Letters_ParentLetterId",
+                table: "Letters");
+
+            migrationBuilder.DropColumn(
+                name: "ChildLetterId",
+                table: "Letters");
+
+            migrationBuilder.DropColumn(
+                name: "ParentLetterId",
+                table: "Letters");
+        }
+    }
+}
