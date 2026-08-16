abfc3783732b4a760778620bd487284f40a4fc7a
Dmitry Mukharski <dmitry9678@gmail.com>
2026-08-16 16:47:19 +0300

    по умолчанию в бд, в spamEmails,  хранится пустой список, вместо null

---
+++ Persistence/Migrations/20260816133720_EmptySpamList.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class EmptySpamList : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<List<string>>(
+                name: "SpamEmails",
+                table: "Users",
+                type: "text[]",
+                nullable: false,
+                oldClrType: typeof(List<string>),
+                oldType: "text[]",
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<List<string>>(
+                name: "SpamEmails",
+                table: "Users",
+                type: "text[]",
+                nullable: true,
+                oldClrType: typeof(List<string>),
+                oldType: "text[]");
+        }
+    }
+}
